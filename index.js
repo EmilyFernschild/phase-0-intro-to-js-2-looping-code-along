42ce7773b008cb374b9fd341964e437d7f3d2de8
Emily Fernschild <emilyfernschild@gmail.com>
2022-08-30 14:30:01 -0400

    commit tree 03e7faf92442f04b59d9403da3ddc7804769fada

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(names, events){
+const messages = [];
+for (let i = 0; i < names.length; i++){
+    const thankYou = `Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+messages.push(thankYou);
+}
+return messages;
+}
+
+function countDown(){
+    let i = 10;
+    while (i >= 0){
+        console.log(i--);
+    }
+    return countDown;
+}
